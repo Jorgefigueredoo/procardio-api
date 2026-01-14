4b99e5287892435c0e42c9a64e984e3c5f7c57a7
Jorgefigueredoo <jorgeafigueredo@gmail.com>
2026-01-14 08:40:05 -0300

    ajustes da função salvar usuário

--- src/main/java/br/com/procardio/api/procardio_api/service/UsuarioService.java
+++ src/main/java/br/com/procardio/api/procardio_api/service/UsuarioService.java
@@ -1,12 +1,15 @@
 package br.com.procardio.api.procardio_api.service;
 
 import java.util.List;
+import java.util.Objects;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.procardio.api.procardio_api.dto.UsuarioDTO;
 import br.com.procardio.api.procardio_api.model.Usuario;
 import br.com.procardio.api.procardio_api.repository.UsuarioRepository;
+import br.com.procardio.api.procardio_api.exceptions.UsuarioNaoEncontradoException;    
 
 @Service
 public class UsuarioService {
@@ -14,10 +17,26 @@ public class UsuarioService {
     @Autowired
     private UsuarioRepository usuarioRepository;
 
-    public Usuario salvarUsuario(Usuario usuario) {
+    public Usuario salvarUsuario(UsuarioDTO usuarioDTO) {
+        Usuario usuario = new Usuario();
+
+        usuario = usuario.toModel(usuarioDTO);
+
         return usuarioRepository.save(usuario);
     }
-    
+
+    public Usuario salvarUsuario(Long id, UsuarioDTO usuarioDTO) {
+        Usuario usuario = buscarUsuarioPorId(id);
+
+        if (Objects.nonNull(usuario)) {
+            usuario = usuario.toModel(usuarioDTO);
+
+            return usuarioRepository.save(usuario);
+        }
+
+        throw new UsuarioNaoEncontradoException(id);
+    }
+
     public void deletarUsuario(Long id) {
         usuarioRepository.deleteById(id);
     }
@@ -28,12 +47,12 @@ public class UsuarioService {
 
     public List<Usuario> bucarUsuariosPorNome(String nome) {
         return usuarioRepository.findByNomeContainingIgnoreCase(nome);
-    }   
+    }
 
     public Usuario buscarUsuarioPorEmail(String email) {
         return usuarioRepository.findByEmail(email).orElse(null);
     }
-    
+
     public List<Usuario> listarTodosUsuarios() {
         return usuarioRepository.findAll();
     }
