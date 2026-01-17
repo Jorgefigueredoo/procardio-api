66ec0cfd86c8b3d6dfb2c84d4704395b66ca1769
Jorgefigueredoo <jorgeafigueredo@gmail.com>
2026-01-17 12:00:54 -0300

    Implementação do UserDetails

--- src/main/java/br/com/procardio/api/procardio_api/model/Usuario.java
+++ src/main/java/br/com/procardio/api/procardio_api/model/Usuario.java
@@ -1,5 +1,13 @@
 package br.com.procardio.api.procardio_api.model;
 
+import java.util.Collection;
+import java.util.List;
+
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import br.com.procardio.api.procardio_api.dto.UsuarioDTO;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embedded;
@@ -15,7 +23,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity(name = "tb_usuarios")
-public class Usuario {
+public class Usuario implements UserDetails{
 
     @Id
     @GeneratedValue (strategy = GenerationType.IDENTITY)
@@ -51,5 +59,40 @@ public class Usuario {
         }
 
         return usuario;
+    }
+
+     @Override
+     public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+     }
+
+     @Override
+     public @Nullable String getPassword() {
+        return senha;
+     }
+
+     @Override
+     public String getUsername() {
+        return email;
+     }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
