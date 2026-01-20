09c9b0b340dd9ca650db2ce3d9bf214b719e86a0
Jorgefigueredoo <jorgeafigueredo@gmail.com>
2026-01-19 21:46:30 -0300

    rota cadastro adicionada

--- src/main/java/br/com/procardio/api/procardio_api/controller/AutenticacaoController.java
+++ src/main/java/br/com/procardio/api/procardio_api/controller/AutenticacaoController.java
@@ -1,6 +1,7 @@
 package br.com.procardio.api.procardio_api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -12,12 +13,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.procardio.api.procardio_api.dto.LoginDTO;
 import br.com.procardio.api.procardio_api.dto.TokenDTO;
+import br.com.procardio.api.procardio_api.dto.UsuarioDTO;
+import br.com.procardio.api.procardio_api.model.Usuario;
 import br.com.procardio.api.procardio_api.service.TokenService;
+import br.com.procardio.api.procardio_api.service.UsuarioService;
 import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/auth")
 public class AutenticacaoController {
+
+    @Autowired
+    private UsuarioService usuarioService;
 
     @Autowired
     private TokenService tokenService;
@@ -35,5 +42,12 @@ public class AutenticacaoController {
         String tokenJWT = tokenService.gerarToken(authentication.getName());
 
         return ResponseEntity.ok(new TokenDTO(tokenJWT, "Bearer"));
+    }
+
+    
+    @PostMapping ("/cadastro")
+    public ResponseEntity<Usuario> cadastrarUsuario(@Valid @RequestBody UsuarioDTO usuarioDTO) {
+        Usuario novoUsuario = usuarioService.salvarUsuario(usuarioDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(novoUsuario);
     }
 }
