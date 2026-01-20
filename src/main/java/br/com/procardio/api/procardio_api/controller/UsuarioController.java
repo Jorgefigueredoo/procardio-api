45165e3b515f4b895e676bf81c6c0a1ce776930f
Jorgefigueredoo <jorgeafigueredo@gmail.com>
2026-01-19 21:47:54 -0300

    ajustes na rota de permissão de atualizar o usuário

--- src/main/java/br/com/procardio/api/procardio_api/controller/UsuarioController.java
+++ src/main/java/br/com/procardio/api/procardio_api/controller/UsuarioController.java
@@ -3,10 +3,9 @@ package br.com.procardio.api.procardio_api.controller;
 import java.util.Objects;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,16 +23,11 @@ public class UsuarioController {
     @Autowired
     private UsuarioService usuarioService;
 
-    @PostMapping
-    public ResponseEntity<Usuario> cadastrarUsuario(@Valid @RequestBody UsuarioDTO usuarioDTO) {
-        Usuario novoUsuario = usuarioService.salvarUsuario(usuarioDTO);
-        return ResponseEntity.status(HttpStatus.CREATED).body(novoUsuario);
-    }
-
     @PutMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN', 'PACIENTE')")
     public ResponseEntity<Usuario> atualizarUsuario(@PathVariable Long id, @Valid @RequestBody UsuarioDTO usuarioDTO) {
         Usuario usuarioAtualizado = usuarioService.salvarUsuario(id, usuarioDTO);
-        
+
         if (Objects.nonNull(usuarioAtualizado)) {
             return ResponseEntity.ok(usuarioAtualizado);
         }
