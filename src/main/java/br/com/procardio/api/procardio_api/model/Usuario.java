1f7c931837a91fe2e7f4c73971ea4c4cc90d6e63
Jorgefigueredoo <jorgeafigueredo@gmail.com>
2026-01-19 21:48:50 -0300

    fazendo as ligações da tabelas com os perfis

--- src/main/java/br/com/procardio/api/procardio_api/model/Usuario.java
+++ src/main/java/br/com/procardio/api/procardio_api/model/Usuario.java
@@ -1,7 +1,9 @@
 package br.com.procardio.api.procardio_api.model;
 
 import java.util.Collection;
-import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.jspecify.annotations.Nullable;
 import org.springframework.security.core.GrantedAuthority;
@@ -9,90 +11,133 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import br.com.procardio.api.procardio_api.dto.UsuarioDTO;
+import br.com.procardio.api.procardio_api.enums.Perfil;
+import jakarta.persistence.CollectionTable;
 import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@NoArgsConstructor
 @AllArgsConstructor
+@NoArgsConstructor
 @Entity(name = "tb_usuarios")
-public class Usuario implements UserDetails{
+
+public class Usuario implements UserDetails {
 
     @Id
-    @GeneratedValue (strategy = GenerationType.IDENTITY)
-    private long id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
     @Column(nullable = false)
     private String nome;
 
-    @Column(nullable = false, unique = true)
+    @Column(nullable = false)
     private String email;
 
-    @Column (nullable = false)
+    @Column(nullable = false)
     private String senha;
 
-    @Embedded
-    private Endereço endereço;
-    
-     public Usuario toModel(UsuarioDTO dto) {
+    @Embedded // Incorporação do endereço como um componente embutido
+    private Endereço endereco;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    // Associação dos perfis de usuário
+    @CollectionTable(name = "tb_perfis", joinColumns = @JoinColumn(name = "usuario_id"))
+    @Enumerated(EnumType.STRING)
+    @Column(name = "perfis")
+    // Set de perfis associados ao usuário (SET não duplica perfis)
+    private Set<Perfil> perfis;
+
+    // Método para adicionar um perfil ao usuário
+    public void adicionarPerfil(Perfil perfil) {
+        this.perfis.add(perfil);
+    }
+
+    // Método para converter UsuarioDTO em Usuario
+    public Usuario toModel(UsuarioDTO dto) {
         Usuario usuario = new Usuario();
 
+        // Mapeamento dos campos do DTO para a entidade
         usuario.setNome(dto.nome());
         usuario.setEmail(dto.email());
         usuario.setSenha(dto.senha());
 
+        if (Objects.nonNull(dto.perfis())) {
+            dto.perfis().stream().forEach(perfil -> {
+                if (Objects.nonNull(perfil)) {
+                    this.adicionarPerfil(perfil);
+                }
+            });
+        }
+
+        // Mapeamento do endereço se fornecido
         if (dto.cep() != null || dto.numero() != null || dto.complemento() != null) {
             Endereço endereco = new Endereço();
 
             endereco.setCep(dto.cep());
             endereco.setNumero(dto.numero());
             endereco.setComplemento(dto.complemento());
-            
-            usuario.setEndereço(endereco);
+
+            usuario.setEndereco(endereco);
         }
 
+        // Retorna a entidade Usuario preenchida
         return usuario;
     }
 
-     @Override
-     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
-     }
+    // Implementação dos métodos da interface UserDetails
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.perfis.stream()
+                .map(perfil -> new SimpleGrantedAuthority(perfil.getRole()))
+                .collect(Collectors.toList());
+    }
 
-     @Override
-     public @Nullable String getPassword() {
-        return senha;
-     }
+    // Implementação do método getPassword
+    @Override
+    public @Nullable String getPassword() {
+        return this.senha;
+    }
 
-     @Override
-     public String getUsername() {
-        return email;
-     }
+    // Implementação do método getUsername
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
 
+    // Implementação do método isAccountNonExpired (conta não expirada)
     @Override
     public boolean isAccountNonExpired() {
         return true;
     }
 
+    // Implementação do método isAccountNonLocked (conta não bloqueada)
     @Override
     public boolean isAccountNonLocked() {
         return true;
     }
 
+    // Implementação do método isCredentialsNonExpired (credenciais não expiradas)
     @Override
     public boolean isCredentialsNonExpired() {
         return true;
     }
 
+    // Implementação do método isEnabled (conta habilitada)
     @Override
     public boolean isEnabled() {
         return true;
     }
+
 }
