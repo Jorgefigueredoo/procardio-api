7b8b3666d6abb52f8bb4eb7eb1ef0251712dcf7e
Jorgefigueredoo <jorgeafigueredo@gmail.com>
2026-01-21 21:24:18 -0300

    public record

--- src/main/java/br/com/procardio/api/procardio_api/dto/TokenDTO.java
+++ src/main/java/br/com/procardio/api/procardio_api/dto/TokenDTO.java
@@ -1,13 +1,3 @@
 package br.com.procardio.api.procardio_api.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-public class TokenDTO {
-    private String token;
-    private String tipo;
-}
+public record TokenDTO(String token) {}
