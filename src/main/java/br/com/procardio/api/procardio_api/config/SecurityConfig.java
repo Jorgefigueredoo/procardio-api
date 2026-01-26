963082e9bb6ce5ed2048c68a781747eed66b1527
Jorgefigueredoo <jorgeafigueredo@gmail.com>
2026-01-26 19:58:42 -0300

    permissão de rota para o front end

--- src/main/java/br/com/procardio/api/procardio_api/config/SecurityConfig.java
+++ src/main/java/br/com/procardio/api/procardio_api/config/SecurityConfig.java
@@ -15,6 +15,9 @@ import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -39,6 +42,19 @@ public class SecurityConfig {
 
     @Autowired
     private TokenService tokenService;
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("http://localhost:3000");
+        configuration.addAllowedMethod("*");
+        configuration.addAllowedHeader("*");
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 
     // Configuração de segurança HTTP
     @Bean
