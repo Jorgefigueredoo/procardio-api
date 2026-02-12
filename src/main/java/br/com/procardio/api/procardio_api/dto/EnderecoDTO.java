bf9b6215f72e992a363076e23da2f2f2186ca0ff
Jorgefigueredoo <jorgeafigueredo@gmail.com>
2026-02-11 21:49:36 -0300

    ajustes de variavéis

--- src/main/java/br/com/procardio/api/procardio_api/dto/EnderecoDTO.java
+++ src/main/java/br/com/procardio/api/procardio_api/dto/EnderecoDTO.java
@@ -1,10 +1,7 @@
 package br.com.procardio.api.procardio_api.dto;
 
 public record EnderecoDTO(
-        String cep,
-        String logradouro,
-        String bairro,
-        String localidade,
-        String uf) {
-
+    String cidade,
+    String estado
+) {
 }
