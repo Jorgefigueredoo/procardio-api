0fa0ffcf843dd3adfdbb7b6a21db920a8fc86b4b
Jorgefigueredoo <jorgeafigueredo@gmail.com>
2026-02-11 21:49:52 -0300

    ...

--- src/main/java/br/com/procardio/api/procardio_api/dto/UsuarioResponseDTO.java
+++ src/main/java/br/com/procardio/api/procardio_api/dto/UsuarioResponseDTO.java
@@ -1,14 +1,10 @@
 package br.com.procardio.api.procardio_api.dto;
 
-import br.com.procardio.api.procardio_api.model.Usuario;
+public record UsuarioResponseDTO(
+        Long id,
+        String nome,
+        String email,
+        EnderecoDTO endereco
+) {
 
-public record UsuarioResponseDTO (
-    
-    Long id, 
-    String nome, 
-    String email) {
-    
-    public UsuarioResponseDTO(Usuario usuario) {
-        this(usuario.getId(), usuario.getNome(), usuario.getEmail());   
-    }
 }
