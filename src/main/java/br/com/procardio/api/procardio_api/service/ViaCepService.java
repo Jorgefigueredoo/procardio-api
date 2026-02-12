4c5121e0336f9ad53e0f196c589c378fd3be6e43
Jorgefigueredoo <jorgeafigueredo@gmail.com>
2026-02-11 21:50:52 -0300

    ajustes finais

--- src/main/java/br/com/procardio/api/procardio_api/service/ViaCepService.java
+++ src/main/java/br/com/procardio/api/procardio_api/service/ViaCepService.java
@@ -2,40 +2,42 @@ package br.com.procardio.api.procardio_api.service;
 
 import java.util.Objects;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import br.com.procardio.api.procardio_api.dto.EnderecoDTO;
-
+import br.com.procardio.api.procardio_api.dto.ViaCepDTO;
 @Service
 public class ViaCepService {
 
     private final String URL_BASE_VIACEP = "https://viacep.com.br/ws/";
-
-    public EnderecoDTO obterDadosEnderecoPeloCep(String cep) {
+    
+    public ViaCepDTO obterDadosEnderecoPeloCep(String cep) {
         if (Objects.isNull(cep) || cep.isBlank()) {
             return null;
         }
 
-        String cepFormatado = cep.replace("\\D", "");
+        String cepFormatado = cep.replaceAll("\\D", "");
 
         if (cepFormatado.length() != 8) {
-            throw new IllegalArgumentException("CEP inválido!");
+            throw new IllegalArgumentException("CEP invalido!");
         }
 
         StringBuilder sb = new StringBuilder();
 
         String url = sb.append(URL_BASE_VIACEP)
-                .append(cepFormatado)
-                .append("/json")
-                .toString();
+                        .append(cepFormatado)
+                        .append("/json")
+                        .toString();
 
         RestTemplate restTemplate = new RestTemplate();
 
         try {
-            return restTemplate.getForObject(url, EnderecoDTO.class);
+            ResponseEntity<ViaCepDTO> endereco = restTemplate.getForEntity(url, ViaCepDTO.class);
+
+            return endereco.getBody();
         } catch (Exception ex) {
-            System.err.println("Erro ao consultar o ViaCEP: " + ex.getMessage());
+            System.err.println("Erro ao consulta ViaCEP: " + ex.getMessage());
             return null;
         }
     }
